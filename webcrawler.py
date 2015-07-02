270f83dd1c3ca11bfa2ca48740bee4ecd256ca39
pedrocunial <pedro_cunial@hotmail.com>
2015-07-02 13:38:41 -0300

    Spider final

--- webcrawler.py
+++ webcrawler.py
@@ -1,16 +1,42 @@
 import requests
 from bs4 import BeautifulSoup
 
-def perfume_spider():
-    url = "http://www.epocacosmeticos.com.br/perfumes"
-    source_code = requests.get(url)
-    plain_text = source_code.text
-    soup = BeautifulSoup(plain_text)
-    for link in soup.findAll('a',{'class': "productImage"}):
-        href = link.get("href")
-        title = link.get("title")
-        print(href, title)
+# def perfume_spider():
+#     url = "http://www.epocacosmeticos.com.br/perfumes"
+#     source_code = requests.get(url)
+#     plain_text = source_code.text
+#     soup = BeautifulSoup(plain_text)
+#     for link in soup.findAll('a',{'class': "productImage"}):
+#         href = link.get("href")
+#         title = link.get("title")
+#         print(href, title)
+#
+# perfume_spider()
+#
+#
+# def corpo_spider():
+#     url = "http://www.epocacosmeticos.com.br/corpo-e-banho"
+#     source_code = requests.get(url)
+#     plain_text = source_code.text
+#     soup = BeautifulSoup(plain_text)
+#     for link in soup.findAll('a',{'class': "productImage"}):
+#         href = link.get("href")
+#         title = link.get("title")
+#         print(href, title)
+#
+# corpo_spider()
 
-perfume_spider()
+def spider():
+    url_base = "http://www.epocacosmeticos.com.br/"
+    complementos = ["perfumes", "maquiagem", "cabelos", "dermocosmeticos", "tratamentos", "corpo-e-banho"]
+    for c in complementos:
+        url = url_base + c
+        source_code = requests.get(url)
+        plain_text = source_code.text
+        soup = BeautifulSoup(plain_text)
+        for link in soup.findAll('a',{'class': "productImage"}):
+            href = link.get("href")
+            title = link.get("title")
+            print(href, title)
 
-
+spider()
