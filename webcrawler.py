9ddd30efd8eb767e5b7e922a24da9e1250da0fe7
pedrocunial <pedro_cunial@hotmail.com>
2015-07-02 13:42:46 -0300

    Limpeza da spider

--- webcrawler.py
+++ webcrawler.py
@@ -1,31 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
 
-# def perfume_spider():
-#     url = "http://www.epocacosmeticos.com.br/perfumes"
-#     source_code = requests.get(url)
-#     plain_text = source_code.text
-#     soup = BeautifulSoup(plain_text)
-#     for link in soup.findAll('a',{'class': "productImage"}):
-#         href = link.get("href")
-#         title = link.get("title")
-#         print(href, title)
-#
-# perfume_spider()
-#
-#
-# def corpo_spider():
-#     url = "http://www.epocacosmeticos.com.br/corpo-e-banho"
-#     source_code = requests.get(url)
-#     plain_text = source_code.text
-#     soup = BeautifulSoup(plain_text)
-#     for link in soup.findAll('a',{'class': "productImage"}):
-#         href = link.get("href")
-#         title = link.get("title")
-#         print(href, title)
-#
-# corpo_spider()
-
 def spider():
     url_base = "http://www.epocacosmeticos.com.br/"
     complementos = ["perfumes", "maquiagem", "cabelos", "dermocosmeticos", "tratamentos", "corpo-e-banho"]
